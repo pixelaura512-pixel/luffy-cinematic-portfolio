29fc916ff30e7b37a403bf623c2bd170b795abda
Pixel Aura <pixelaura512@gmail.com>
2026-09-12 13:07:51 +0500

    Prepare site for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/luffy-cinematic-portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
